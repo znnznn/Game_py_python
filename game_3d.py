2997af063c06163117597b1590f3dc38f37ee742
znnznn <znnznn@ukr.net>
2021-01-03 00:21:17 +0200

    покрищив подуктивність, додав клас промальовки і додав маленьку карту

--- game_3d.py
+++ game_3d.py
@@ -1,12 +1,17 @@
 import pygame
 import math
 
+""" game setting """
 WIDTH = 1200
 HEIGHT = 800
 HALF_WIDTH = WIDTH // 2
 HALF_HEIGHT = HEIGHT // 2
+TILE = 100
 FPS = 60
+FPS_POS = (WIDTH - 65, 5)
 
+
+""" colors """
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 RED = (220, 0, 0)
@@ -14,22 +19,29 @@ GREEN = (0, 220, 0)
 BLUE = (0, 0, 220)
 DARKGREY = (110, 110, 100)
 PURPLE = (120, 0, 120)
+SKYBLUEE = (0, 186, 255)
+YELLOW = (220, 220, 0)
 
+""" player setting """
 player_pos = (HALF_WIDTH, HALF_HEIGHT)
 player_angle = 0
 player_speed = 2
 
 """ область бачення """
-TILE = 100
+
 FOV = math.pi / 3
 HALF_FOV = FOV / 2
-NUM_RAYS = 120
+NUM_RAYS = 300
 MAX_DEPTH = 800
 DELTA_ANGLE = FOV / NUM_RAYS
 DIST = NUM_RAYS / (2 * math.tan(HALF_FOV))
-PROJ_COEFF = 3 * DIST * TILE
+PROJ_COEFF = DIST * TILE
 SCALE = WIDTH // NUM_RAYS
 
+""" mini map setting """
+MAP_SCALE = 5
+MAP_TILE = TILE // MAP_SCALE
+MAP_POS =(0, HEIGHT - HEIGHT // MAP_SCALE)
 
 text_map = [
     'WWWWWWWWWWWW',
@@ -43,17 +55,17 @@ text_map = [
 ]
 
 word_map = set()
+mini_map = set()
 for j, row in enumerate(text_map):
     for i, char in enumerate(row):
         if char == 'W':
             word_map.add((i * TILE, j * TILE))
+            mini_map.add((i * MAP_TILE, j * MAP_TILE))
 
 class Player:
-
     def __init__(self):
         self.x, self.y = player_pos
         self.angle = player_angle
-
 
     @property
     def pos(self):
@@ -85,29 +97,80 @@ class Player:
             self.angle += 0.02
 
 
+class Drawing:
+    def __init__(self, sc,map_sc):
+        self.screen = sc
+        self.sc_map = map_sc
+        self.font = pygame.font.SysFont('Arial', 38, bold=True)
+
+    def background(self):
+        pygame.draw.rect(self.screen, SKYBLUEE, (0, 0, WIDTH, HALF_HEIGHT))
+        pygame.draw.rect(self.screen, DARKGREY, (0, HALF_HEIGHT, WIDTH, HALF_HEIGHT))
+
+    def word(self, player_pos, player_angle):
+        ray_casting(self.screen, player_pos, player_angle)
+
+    def fps(self, clock):
+        display_fps = str(int(clock.get_fps()))
+        render = self.font.render(display_fps, True, RED)
+        self.screen.blit(render, FPS_POS)
+
+    def mini_map(self, player):
+        self.sc_map.fill(BLACK)
+        map_x, map_y = player.x // MAP_SCALE, player.y // MAP_SCALE
+
+        pygame.draw.line(self.sc_map, YELLOW, (map_x, map_y), ((map_x + 12 * math.cos(player.angle)),
+                                                               (map_y + 12 * math.sin(player.angle))), 2)
+        pygame.draw.circle(self.sc_map, RED, (int(map_x), int(map_y)), 5)
+        for x, y in mini_map:
+            pygame.draw.rect(self.sc_map, GREEN, (x, y, MAP_TILE, MAP_TILE))
+        self.screen.blit(self.sc_map, MAP_POS)
+
+
+def mapping(a, b):
+    return (a // TILE) * TILE, (b // TILE) * TILE
+
+
 def ray_casting(screen, player_pos, player_angle):
+    ox, oy = player_pos
+    xm, ym = mapping(ox, oy)
     cur_angle = player_angle - HALF_FOV
-    xo, yo = player_pos
     for ray in range(NUM_RAYS):
         cos_a = math.cos(cur_angle)
         sin_a = math.sin(cur_angle)
-        for depth in range(MAX_DEPTH):
-            x = xo + depth * cos_a
-            y = yo + depth * sin_a
-            if (x // TILE * TILE, y // TILE * TILE) in word_map:
-                depth *= math.cos(player_angle - cur_angle)
-                c = 255 / (1 + depth * depth * 0.0001)
-                color = (int(c), int(c), int(c))
-                proj_height = PROJ_COEFF / depth
-                pygame.draw.rect(screen, color, (ray * SCALE, HALF_HEIGHT - proj_height // 2, SCALE, proj_height))
+        """ вертикаль """
+        x, dx = (xm + TILE, 1) if cos_a >= 0 else (xm, -1)
+        for i in range(0, WIDTH, TILE):
+            depth_v = (x - ox) / cos_a
+            y = oy + depth_v * sin_a
+            if mapping(x + dx, y) in word_map:
                 break
-        #pygame.draw.line(screen, DARKGREY, player_pos, (x, y), 2)
+            x += dx * TILE
+        """ горізнталь """
+        y, dy = (ym + TILE, 1) if sin_a >= 0 else (ym, -1)
+        for i in range(0, HEIGHT, TILE):
+            depth_h = (y - oy) / sin_a
+            x = ox + depth_h * cos_a
+            if mapping(x, y + dy) in word_map:
+                break
+            y += dy * TILE
+
+        """ проекція """
+        depth = depth_v if depth_v < depth_h else depth_h
+        depth *= math.cos(player_angle - cur_angle)
+        c = 255 / (1 + depth * depth * 0.00002)
+        color = (int(c), int(c // 2), int(c // 3))
+        proj_height = PROJ_COEFF / (depth + 0.00001)
+        pygame.draw.rect(screen, color, (ray * SCALE, HALF_HEIGHT - proj_height // 2, SCALE, proj_height))
         cur_angle += DELTA_ANGLE
+
 
 pygame.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
+map_screen = pygame.Surface((WIDTH // MAP_SCALE, HEIGHT // MAP_SCALE))
 clock = pygame.time.Clock()
 player = Player()
+drawing = Drawing(screen, map_screen)
 
 while True:
     for event in pygame.event.get():
@@ -115,13 +178,10 @@ while True:
             exit()
     player.movement()
     screen.fill(BLACK)
-    pygame.draw.rect(screen, BLUE, (0,0,WIDTH, HALF_HEIGHT))
-    pygame.draw.rect(screen, DARKGREY, (0, HALF_HEIGHT, WIDTH, HALF_HEIGHT))
-    ray_casting(screen, player.pos, player.angle)
-    # pygame.draw.circle(screen, GREEN, (int(player.x), int(player.y)), 12)
-    # pygame.draw.line(screen, GREEN, player.pos, ((player.x + WIDTH * math.cos(player.angle)),
-    #                                              (player.y + WIDTH * math.sin(player.angle))))
-    # for x,y in word_map:
-    #     pygame.draw.rect(screen, DARKGREY, (x, y, TILE, TILE), 2)
+    drawing.background()
+    drawing.word(player.pos, player.angle)
+    drawing.fps(clock)
+    drawing.mini_map(player)
+
     pygame.display.flip()
-    clock.tick(60)
+    clock.tick()
